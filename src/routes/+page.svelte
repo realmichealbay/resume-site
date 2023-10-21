19bc966fc5ded5e6c6746d1a3ef270b30eafd796
Realmichealbay1 <realmichealbay1@gmail.com>
2023-10-21 16:38:53 -0500

    commit on master

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,6 @@
 <div id="intro-container">
 	<div id="over-top-intro">
 	<div>
-		<h1>Bonjour, je m'appelle Ethan Wallace.</h1>
-	</div>
-	<div>
-		<h1>Hallo, mein Name ist Ethan Wallace.</h1>
-	</div>
-	<div>
-		<h1>Olá, meu nome é Ethan Wallace.</h1>
-	</div>
-	<div>
-		<h1>こんにちは、私の名前はイーサン・ウォレスです</h1>
-	</div>
-	<div>
-		<h1>Привет, меня зовут Итан Уоллес</h1>
-	</div>
-	<div>
-		<h1>مرحبًا اسمي إيثان والاس</h1>
-	</div>
-	<div>
 		<h1>Hi, my name is Ethan Wallace.</h1>
 	</div>
 </div>
