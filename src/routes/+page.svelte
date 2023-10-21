cbbe455ecf02f99a5b81127b647b2bce12a4b873
Realmichealbay1 <realmichealbay1@gmail.com>
2023-10-20 20:32:53 -0500

    add on master

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,34 @@
 
 
 <div id="over-top-intro">
-    <div class="">
-
+    <div>
+        <h1>Hola, mi nombre es Ethan Wallace.</h1>
     </div>
-    <h1>Hola, mi nombre es Ethan Wallace.</h1>
-
-    <h1>Salut, je m'appelle Ethan Wallace</h1>
-    <h1>Bonjour, je m'appelle Ethan Wallace.</h1>
-    <h1>Hallo, mein Name ist Ethan Wallace.</h1>
-    <h1>Ciao, mi chiamo Ethan Wallace.</h1>
-    <h1>Olá, meu nome é Ethan Wallace.</h1>
-    <h1>こんにちは、私の名前はイーサン・ウォレスです</h1>
-    <h1>Привет, меня зовут Итан Уоллес</h1>
-    <h1>مرحبًا اسمي إيثان والاس</h1>
-    <h1>Hi, my name is Ethan Wallace.</h1>
+    <div>
+        <h1>Salut, je m'appelle Ethan Wallace</h1>
+    </div>
+    <div>
+        <h1>Bonjour, je m'appelle Ethan Wallace.</h1>
+    </div>
+    <div>
+        <h1>Hallo, mein Name ist Ethan Wallace.</h1>
+    </div>
+    <div>
+        <h1>Ciao, mi chiamo Ethan Wallace.</h1>
+    </div>
+    <div>
+        <h1>Olá, meu nome é Ethan Wallace.</h1>
+    </div>
+    <div>
+        <h1>こんにちは、私の名前はイーサン・ウォレスです</h1>
+    </div>
+    <div>
+        <h1>Привет, меня зовут Итан Уоллес</h1>
+    </div>
+    <div>
+        <h1>مرحبًا اسمي إيثان والاس</h1>
+    </div>
+    <div>
+        <h1>Hi, my name is Ethan Wallace.</h1>
+    </div>
 </div>
